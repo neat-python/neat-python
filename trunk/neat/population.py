7ea5fb2c17b6ee50206b17e7fb352f1e52b93e27
cesar.gomes <cesar.gomes@3a2a2d67-9b3a-0410-8382-8b98c2212edd>
2007-10-24 19:31:45 +0000

    Removi o deepcopy

--- trunk/neat/population.py
+++ trunk/neat/population.py
@@ -6,7 +6,7 @@ else:
 from species import *
 import copy
 #from psyco.classes import *
-    
+
 class Population:
     ''' Manages all the species  '''
     evaluate = None # Evaluates the entire population. You need to override 
@@ -17,8 +17,7 @@ class Population:
         self.__population = [Chromosome.create_fully_connected(Config.input_nodes, Config.output_nodes) \
                              for i in xrange(self.__popsize)]
         self.__species = []
-        self.compatibility_threshold = Config.compatibility_threshold
-        
+                
         # Statistics
         self.__avg_fitness = []
         self.__best_fitness = []
@@ -33,54 +32,43 @@ class Population:
     
     def remove(self, chromo):
         ''' Removes a chromosome from the population '''
-        self.__population.remove(chromo)
+        self.__population.remove(chromo)      
         
     def __speciate(self):
         """ Group chromosomes into species by similarity """
         
-        if len(self.__species) > 0:   
-            # put best members back to their species
-            for c in self:
-                for s in self.__species:                
-                    if c.id == s.representative.id:
-                        x = copy.deepcopy(c)
-                        s.add(x) 
-                        break
-                    
-            # Remove empty species
-            self.__species = [s for s in self.__species if len(s) > 0] 
-                        
-            # remove each species' representant from population        
-            for s in self.__species:
-                for c in self:
-                    if c.id == s.representative.id:
-                        self.remove(c)
-                        break        
-            
-        # Speciate the remaining population
+        # Speciate the population
         for c in self:
-            found = False
-            # TODO: if c.species_id is not None try this species first      
-            for s in self.__species:      
-                # or random.choice(s.chromosomes) ?          
-                if c.distance(s.chromosomes[0]) < self.compatibility_threshold:
-                    x = copy.deepcopy(c)
-                    s.add(x)                              
-                    #print 'chromo %s added to species %s' %(chromo.id, s.id)
+            found = False    
+            for s in self.__species:
+                if s.representative.id == c.species_id:
+                    s.add(c)
                     found = True
-                    break # we found a compatible species, so let's skip to the next
+                    break    
+                # if c.species_id is not None try this species first  
+                if c.species_id == s.id and not found:
+                    if c.distance(s.representative) < Config.compatibility_threshold:    
+                        s.add(c)
+                        found = True
+                        break
+            
+            if not found:
+                for s in self.__species:      
+                    if c.distance(s.representative) < Config.compatibility_threshold:
+                        s.add(c)                               
+                        #print 'chromo %s added to species %s' %(c.id, s.id)
+                        found = True
+                        break # we found a compatible species, so let's skip to the next
                 
             if not found: # create a new species for this lone chromosome
-                x = copy.deepcopy(c)
-                self.__species.append(Species(x))                                 
-                #print 'Creating new species %s and adding chromo %s' %(self.__species[-1].id, c.id)
-        
+                self.__species.append(Species(c))  
+       
         # controls compatibility threshold
         if len(self.__species) > species_size:
-            self.compatibility_threshold += compatibility_change
+            Config.compatibility_threshold += Config.compatibility_change
         elif len(self.__species) < species_size:
-            if self.compatibility_threshold > compatibility_change:
-                self.compatibility_threshold -= compatibility_change
+            if Config.compatibility_threshold > Config.compatibility_change:
+                Config.compatibility_threshold -= Config.compatibility_change
             else:
                 print 'Compatibility threshold cannot be changed (minimum value has been reached)'            
                 
@@ -127,26 +115,30 @@ class Population:
         
         for i, s in enumerate(self.__species):
             s.spawn_amount = int(round((species_stats[i]*self.__popsize/total_average)))
+            if s.spawn_amount == 0:
+                # This rarely happens
+                print 'Species %d (age %s) will be removed (produced no offspring)' %(s.id, s.age)
                     
-    def epoch(self, n):
+    def epoch(self, n, stats=True):
         ''' Runs NEAT's genetic algorithm for n epochs. All the speciation methods are handled here '''
         
         for generation in xrange(n):
             print 'Running generation',generation
             
             # Evaluate individuals
-            self.evaluate()              
-            # Current generation's best chromosome 
-            self.__best_fitness.append(copy.deepcopy(max(self.__population))) # reference?
-            # Current population's average fitness
-            self.__avg_fitness.append(self.average_fitness())                              
-            # Speciates the population
+            self.evaluate()     
+             # Speciates the population
             self.__speciate()          
             # Compute spawn levels for each remaining species
-            self.__compute_spawn_levels()      
+            self.__compute_spawn_levels()                   
+            # Removing species with spawn amount = 0
+            self.__species = [s for s in self.__species if s.spawn_amount > 0]                        
+            # Current generation's best chromosome 
+            self.__best_fitness.append(max(self.__population))
+            # Current population's average fitness
+            self.__avg_fitness.append(self.average_fitness())     
 
-
-            best = self.__best_fitness[-1] # temporary reference (just to print some stats)
+            best = self.__best_fitness[-1] # just to print some stats
             # Which species has the best chromosome?
             for s in self.__species:
                 s.hasBest = False
@@ -154,25 +146,26 @@ class Population:
                     s.hasBest = True
 
             print 'Population\'s average fitness', self.__avg_fitness[-1]
-            print 'Best fitness: %s - size: %s - species %s' \
-                    %(best.fitness, best.size(), best.species_id)
+            print 'Best fitness: %s - size: %s - species %s - id %s' \
+                    %(best.fitness, best.size(), best.species_id, best.id)
             
             # print best_chromo
             
-            if best.fitness > 0.9999:
+            if best.fitness > Config.max_fitness_threshold:
                 file = open('best','w')
                 file.write(str(best))
                 file.close()
                 break
            
-            # print some "debugging" information
-            print 'Species length: %d totalizing %d individuals' \
-                    %(len(self.__species), sum([len(s) for s in self.__species]))
-            print 'Species ID :',[s.id for s in self.__species]
-            print 'Each species size:', [len(s) for s in self.__species]
-            print 'Amount to spawn:',[s.spawn_amount for s in self.__species]
-            print 'Species age:',[s.age for s in self.__species]
-            print 'Species imp:',[s.no_improvement_age for s in self.__species] # species no improvement age            
+            if stats:
+                # print some "debugging" information
+                print 'Species length: %d totalizing %d individuals' \
+                        %(len(self.__species), sum([len(s) for s in self.__species]))
+                print 'Species ID :',[s.id for s in self.__species]
+                print 'Each species size:', [len(s) for s in self.__species]
+                print 'Amount to spawn:',[s.spawn_amount for s in self.__species]
+                print 'Species age:',[s.age for s in self.__species]
+                print 'Species imp:',[s.no_improvement_age for s in self.__species] # species no improvement age            
                 
             # -------------------------- Producing new offspring -------------------------- #
             new_population = [] # next generation's population
@@ -182,50 +175,57 @@ class Population:
                 #print 'Species %s produced %s' %(s.id, len(temp))
                 new_population.extend(s.reproduce())
                            
-            # Controls under or overflow
-            fill = (self.__popsize) - len(new_population)
-            if fill < 0: # overflow
-                print 'Removing %d excess individual(s) from the new population' %-fill
-                # This is dangerous! I can't remove a species' representative!
-                new_population = new_population[:fill] # Removing the last added members
-                
-            if fill > 0: # underflow
-                print 'Selecting %d more individual(s) to fill up the new population' %fill
-                # Apply tournament selection in the whole population (allow inter-species mating?)
-                # or select a random species to reproduce?
-                for i in range(fill):
-#                    parent1 = self.TournamentSelection() 
-#                    parent2 = self.TournamentSelection()
-#                    child = parent1.crossover(parent2)
-#                    # child = max(self.__population) - only apply mutations (give better results?)
-#                    new_population.append(child.mutate())    
-
-                    # Selects a random chromosome from population                    
-                    parent1 = random.choice(self.__population)                    
-                    # Search for a mate within same species
-                    found = False
-                    for c in self.__population:
-                        if c.species_id == parent1.species_id:
-                            child = parent1.crossover(c)
-                            new_population.append(child.mutate())
-                            found = True
-                            break
-                    # If found no mate, just mutate it.
-                    if not found:
-                        new_population.append(parent1.mutate())
-                                 
-                            
-                    
-            # Updates current population
-            assert self.__popsize == len(new_population), 'Different population sizes!'
+#            # Controls under or overflow
+#            fill = (self.__popsize) - len(new_population)
+#            if fill < 0: # overflow
+#                print 'Removing %d excess individual(s) from the new population' %-fill
+#                # This is dangerous! I can't remove a species' representative!
+#                new_population = new_population[:fill] # Removing the last added members
+#                
+#            if fill > 0: # underflow
+#                print 'Selecting %d more individual(s) to fill up the new population' %fill
+#                # Apply tournament selection in the whole population
+#                # or select a random species to reproduce?
+#                for i in range(fill):
+##                    parent1 = self.TournamentSelection() 
+##                    parent2 = self.TournamentSelection()
+##                    child = parent1.crossover(parent2)
+##                    # child = max(self.__population) - only apply mutations (give better results?)
+##                    new_population.append(child.mutate())                    
+#
+#                    # Selects a random chromosome from population                    
+#                    parent1 = random.choice(self.__population)                    
+#                    # Search for a mate within the same species
+#                    found = False
+#                    for c in self.__population:
+#                        if c.species_id == parent1.species_id:
+#                            child = parent1.crossover(c)
+#                            new_population.append(child.mutate())
+#                            found = True
+#                            break
+#                    # If found no mate, just mutate it.
+#                    if not found:
+#                        new_population.append(parent1.mutate())
+#                    
+#            # Updates current population
+#            assert self.__popsize == len(new_population), 'Different population sizes!'
             self.__population = new_population[:]
+            
             # The new pop hasn't been evaluated at this point! Don't call average_fitness() !
             
             # Remove stagnated species (except if it has the best chromosome)
             self.__species = [s for s in self.__species if \
                               s.no_improvement_age <= max_stagnation or \
-                              s.no_improvement_age > max_stagnation and s.hasBest == True]    
-        
+                              s.no_improvement_age > max_stagnation and s.hasBest == True] 
+            
+            for s in self.__species:
+                if s.no_improvement_age > 50:
+                    print 'Species %d is super-stagnated, removing it' %(s.id)
+            
+            # Remove super-stagnated species (even if it has the best chromosome)
+            self.__species = [s for s in self.__species if \
+                              s.no_improvement_age < 50]
+
 if __name__ ==  '__main__' :
     
     # sample fitness function
