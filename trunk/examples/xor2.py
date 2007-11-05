2a2569893d4263c7dbfa0a943cd6aff2268b3165
cesar.gomes <cesar.gomes@3a2a2d67-9b3a-0410-8382-8b98c2212edd>
2007-11-05 00:05:47 +0000

    Não funciona usando a métrica de erro do Stanley! Por quê?

--- trunk/examples/xor2.py
+++ trunk/examples/xor2.py
@@ -8,7 +8,7 @@ config.Config.output_nodes = 1
 config.Config.prob_addconn = 0.03
 config.Config.prob_addnode = 0.05
 config.Config.survival_threshold = 0.1
-config.Config.max_fitness_threshold = 0.9
+config.Config.max_fitness_threshold = 14.9
 config.Config.compatibility_change = 0.0
 config.Config.nn_allow_recurrence = False
 config.Config.nn_activation = 'exp'
@@ -27,18 +27,16 @@ def eval_fitness(population):
         error = 0.0
         for i, input in enumerate(INPUTS):
             output = brain.activate(input) # serial activation
-            error += (output[0] - OUTPUTS[i])**2
-            #error += math.fabs(output[0] - OUTPUTS[i])
-        #chromosome.fitness = (4.0 - error)**2 # (Stanley p. 43)        
-        chromosome.fitness = 1 - math.sqrt(error/len(OUTPUTS))
+            #error += (output[0] - OUTPUTS[i])**2
+            error += math.fabs(output[0] - OUTPUTS[i])
+        chromosome.fitness = (4.0 - error)**2 # (Stanley p. 43)        
+        #chromosome.fitness = 1 - math.sqrt(error/len(OUTPUTS))
         
 population.Population.evaluate = eval_fitness
 pop = population.Population(150)
-pop.epoch(200, stats=True)
+pop.epoch(200, stats=True, save_best=False)
 
-# Requires: PyDot -  http://code.google.com/p/pydot/downloads/list
-# very, very, very draft solution for network visualizing
+# Draft solution for network visualizing
 visualize.draw_net(pop.stats[0][-1]) # best chromosome
-# visualize.draw_net(max(pop.stats[0])) # must be the same as pop.stats[-1]
-# Requires: biggles - http://biggles.sourceforge.net/
+# Plots the evolution of the best/average fitness
 visualize.plot_stats(pop.stats)
