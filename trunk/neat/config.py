e41d356f0ff3194beeedb3416501b0b6bdfcbfad
cesar.gomes <cesar.gomes@3a2a2d67-9b3a-0410-8382-8b98c2212edd>
2008-01-28 23:33:29 +0000

    Parsing para carregar um arquivo de configuração.

--- trunk/neat/config.py
+++ trunk/neat/config.py
@@ -1,60 +1,99 @@
 # sets the configuration parameters for NEAT
+from ConfigParser import ConfigParser
        
 def load(file):
     try:
-        parameters = open('file','r')
+        config_file = open(file,'r')
     except IOError:
         print 'Error: file %s not found!' %file
-    #else:
+    else:        
+        parameters = ConfigParser()
+        parameters.readfp(config_file)
+        
         # set class attributes
         
+        # phenotype
+        Config.input_nodes          =       int(parameters.get('phenotype','input_nodes'))
+        Config.output_nodes         =       int(parameters.get('phenotype','output_nodes'))
+        Config.max_weight           =     float(parameters.get('phenotype','max_weight'))
+        Config.min_weight           =     float(parameters.get('phenotype','min_weight'))
+        Config.nn_allow_recurrence  =  bool(int(parameters.get('phenotype','nn_allow_recurrence')))
+        Config.nn_activation        =           parameters.get('phenotype','nn_activation')  # exp or tanh
+        Config.random_range         =     float(parameters.get('phenotype','random_range'))
+        
+        # GA
+        Config.pop_size                 =   int(parameters.get('genetic','pop_size'))
+        Config.max_fitness_threshold    = float(parameters.get('genetic','max_fitness_threshold'))
+        Config.prob_addconn             = float(parameters.get('genetic','prob_addconn'))
+        Config.prob_addnode             = float(parameters.get('genetic','prob_addnode'))
+        Config.prob_mutatebias          = float(parameters.get('genetic','prob_mutatebias'))
+        Config.bias_mutation_power      = float(parameters.get('genetic','bias_mutation_power'))
+        Config.prob_mutate_weight       = float(parameters.get('genetic','prob_mutate_weight'))
+        Config.weight_mutation_power    = float(parameters.get('genetic','weight_mutation_power'))
+        Config.prob_togglelink          = float(parameters.get('genetic','prob_togglelink'))
+        
+        # genotype compatibility
+        Config.compatibility_threshold  = float(parameters.get('genotype compatibility','compatibility_threshold'))
+        Config.compatibility_change     = float(parameters.get('genotype compatibility','compatibility_change'))
+        Config.excess_coeficient        = float(parameters.get('genotype compatibility','excess_coeficient'))
+        Config.disjoint_coeficient      = float(parameters.get('genotype compatibility','disjoint_coeficient'))
+        Config.weight_coeficient        = float(parameters.get('genotype compatibility','weight_coeficient'))
+          
+        # species
+        Config.species_size         =   int(parameters.get('species','species_size'))
+        Config.survival_threshold   = float(parameters.get('species','survival_threshold'))
+        Config.old_threshold        =   int(parameters.get('species','old_threshold'))
+        Config.youth_threshold      =   int(parameters.get('species','youth_threshold'))
+        Config.old_penalty          = float(parameters.get('species','old_penalty'))    # always in (0,1)
+        Config.youth_boost          = float(parameters.get('species','youth_boost'))   # always in (1,2)
+        Config.max_stagnation       =   int(parameters.get('species','max_stagnation'))
+        
+class Config: 
 
-class Config: # read from file
-    # network type
-    nn_allow_recurrence = False
-    nn_activation = 'exp'  # exp or tanh
-    
     # phenotype config
-    input_nodes = 2
-    output_nodes = 1
-    max_weight = 500
-    min_weight = -500
-    random_range = 20.5 # experimental
-    
-    # mutation probabilities
-    prob_addconn = 0.05
-    prob_addnode = 0.03    
-    prob_mutatebias = 0.2
-    bias_mutation_power = 0.5    
-    prob_mutate_weight = 0.25 # dynamic mutation rate (future release)
-    weight_mutation_power = 1.5    
-    prob_togglelink = 0.1
+    input_nodes         = None
+    output_nodes        = None
+    max_weight          = None
+    min_weight          = None
+    nn_allow_recurrence = None
+    nn_activation       = None
+    random_range        = None
+     
+    # GA config
+    pop_size                = None
+    max_fitness_threshold   = None
+    prob_addconn            = None
+    prob_addnode            = None   
+    prob_mutatebias         = None
+    bias_mutation_power     = None
+    prob_mutate_weight      = None # dynamic mutation rate (future release)
+    weight_mutation_power   = None
+    prob_togglelink         = None
     
     #prob_crossover = 0.7  # not implemented (always apply crossover)
     #prob_weightreplaced = 0.0 # not implemented
     
-    # genetic algorithm parameters
-    #pop_size = 50        # set when initializing population
-    #number_epochs = 1000 # set when initializing population
-    max_fitness_threshold = 0.99
-    
     # genotype compatibility 
-    compatibility_threshold = 3.0
-    compatibility_change = 0.0
-    excess_coeficient = 1.0
-    disjoint_coeficient = 1.0
-    weight_coeficient = 0.4
+    compatibility_threshold = None
+    compatibility_change    = None
+    excess_coeficient       = None
+    disjoint_coeficient     = None
+    weight_coeficient       = None
     
     # species
-    species_size = 10
-    survival_threshold = 0.1 # only the best 20% for each species is allowed to mate
-    old_threshold = 80
-    youth_threshold = 10
-    old_penalty = 0.2    # always in (0,1)
-    youth_boost = 1.7    # always in (1,2)
-    max_stagnation = 15
+    species_size        = None
+    survival_threshold  = None # only the best 20% for each species is allowed to mate
+    old_threshold       = None
+    youth_threshold     = None
+    old_penalty         = None    # always in (0,1)
+    youth_boost         = None    # always in (1,2)
+    max_stagnation      = None
     
     # for a future release
     #ele_event_time = 1000
     #ele_events = False            
+                
+
+        
+          
                 
