a5a33030acc85d4f335981f0d73c172e65969d89
cesar.gomes <cesar.gomes@3a2a2d67-9b3a-0410-8382-8b98c2212edd>
2008-02-20 01:10:00 +0000

    As mutações paramétricas devem ocorrer em seus respectivos genes.

--- branches/neat_revisited/neat/genome2.py
+++ branches/neat_revisited/neat/genome2.py
@@ -13,6 +13,11 @@ class NodeGene(object):
         self._response = response
         assert(self._type in ('INPUT', 'OUTPUT', 'HIDDEN'))
         
+    id = property(lambda self: self._id)
+    type = property(lambda self: self._type)
+    bias = property(lambda self: self._bias)
+    response = property(lambda self: self._response)
+        
     def __str__(self):
         return "Node %2d %6s, bias %+2.5s, response %+2.5s"% (self._id, self._type, self._bias, self._response)
     
@@ -25,35 +30,40 @@ class NodeGene(object):
                       random.choice((self._response, other._response)))
         return ng
     
-    def mutate_bias(self):
+    def __mutate_bias(self):
         self._bias += random.uniform(-1, 1) * Config.bias_mutation_power
         if self._bias > Config.max_weight:
             self._bias = Config.max_weight
         elif self._bias < Config.min_weight:
             self._bias = Config.min_weight
-        return self
     
-    def mutate_response(self):
+    def __mutate_response(self):
     	''' Mutates the neuron's firing response. '''
-        self._response += random.uniform(-0.5, 0.5)
-        return self
+        self._response += random.uniform(-0.2, 0.2) * Config.bias_mutation_power
     
     def copy(self):
         return NodeGene(self._id, self._type, self._bias, self._response)
-    
-    id = property(lambda self: self._id)
-    type = property(lambda self: self._type)
-    bias = property(lambda self: self._bias)
+      
+    def mutate(self):
+        r = random.random
+        if r() < Config.prob_mutatebias:
+            self.__mutate_bias()
+        if r() < Config.prob_mutatebias:
+            self.__mutate_response()
     
 
 class CTNodeGene(NodeGene):
     '''Continuous-time node gene - used in CTRNNs '''
-    def __init__(self, id, nodetype, bias, response, time_constant=1.0):
+    def __init__(self, id, nodetype, bias=0, response=4.924273, time_constant=1.0):
         super(CTNodeGene, self).__init__(id, nodetype, bias, response)
         
         self._time_constant = time_constant
         
-    def mutate_time_constant(self):
+    def mutate(self):
+        super(CTNodeGene, self).mutate()
+        self.__mutate_time_constant()
+        
+    def __mutate_time_constant(self):
         self._time_constant += random.uniform(-1, 1) * Config.bias_mutation_power
         if self._time_constant > Config.max_weight:
             self._time_constant = Config.max_weight
@@ -99,9 +109,30 @@ class ConnectionGene(object):
     # Key for dictionaries, avoids two connections between the same nodes.
     key = property(lambda self: (self.__in, self.__out))
     
-    def enable(self):
+    def mutate(self):
+        r = random.random
+        if r() <  Config.prob_togglelink:
+            self.__enable()
+        if r() < Config.prob_mutate_weight: 
+            self.__mutate_weight()
+        if r() < 0.1:
+            self.__weight_replaced()
+        
+    
+    def __enable(self):
         '''For the "enable link" mutation'''
         self.__enabled = True
+
+    def __mutate_weight(self):
+        self.__weight += random.uniform(-1,1) * Config.weight_mutation_power
+        
+        if self.__weight > Config.max_weight:
+            self.__weight = Config.max_weight
+        elif self.__weight < Config.min_weight:
+            self.__weight = Config.min_weight
+        
+    def __weight_replaced(self):
+        self.__weight = random.uniform(-Config.random_range, Config.random_range)
     
     @classmethod
     def __get_new_innov_number(cls):
@@ -125,14 +156,6 @@ class ConnectionGene(object):
         new_conn1 = ConnectionGene(self.__in, node_id, 1, True)
         new_conn2 = ConnectionGene(node_id, self.__out, self.__weight, True)
         return new_conn1, new_conn2
-    
-    def mutate_weight(self):
-        self.__weight += random.uniform(-1, 1) * Config.weight_mutation_power
-        
-        if self.__weight > Config.max_weight:
-            self.__weight = Config.max_weight
-        elif self.__weight < Config.min_weight:
-            self.__weight = Config.min_weight
     
     def copy(self):
         return ConnectionGene(self.__in, self.__out, self.__weight,
