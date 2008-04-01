3e3f98a29a7b57b9072930b4eddcecdd8f458d9e
cesar.gomes <cesar.gomes@3a2a2d67-9b3a-0410-8382-8b98c2212edd>
2008-04-01 23:10:53 +0000

    Deleting obsolete version.

--- trunk/setup.py
+++
@@ -1,8 +0,0 @@
-# Installation script
-from distutils.core import setup
-setup(
-      name='neat-python',
-      version='0.1',
-      description='A NEAT (NeuroEvolution of Augmenting Topologies) implementation',
-      packages=['neat'],
-)
